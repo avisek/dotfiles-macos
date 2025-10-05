1c4e0fb3862d5715a22009cbb7735b03a5fc97c7
Avisek Das <avisekdas555@gmail.com>
2025-10-05 19:02:18 +0530

    add git email configuration for symbiofy org in git.nix

--- home/git.nix
+++ home/git.nix
@@ -11,6 +11,17 @@
       init.defaultBranch = "main";
       push.autoSetupRemote = true;
     };
+
+    includes = [
+      {
+        condition = "gitdir:~/Work/symbiofy/";
+        contents = {
+          user = {
+            email = "avisek@symbiofy.ai";
+          };
+        };
+      }
+    ];
   };
 
   # echo "n" | gh auth login --git-protocol https --web --skip-ssh-key --clipboard
