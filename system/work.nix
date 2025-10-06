169e4a8aeea556ed7475ac14b917fb393b313efb
Avisek Das <avisekdas555@gmail.com>
2025-10-06 21:27:42 +0530

    disable slack auto update

--- system/work.nix
+++ system/work.nix
@@ -2,4 +2,10 @@
   environment.systemPackages = with pkgs; [
     slack
   ];
+
+  system.defaults.CustomUserPreferences = {
+    "com.tinyspeck.slackmacgap" = {
+      AutoUpdate = false;
+    };
+  };
 }
