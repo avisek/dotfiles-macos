e95a7c2201bacf975e68b80a8c704b5427af3e48
Avisek Das <avisekdas555@gmail.com>
2025-10-09 03:11:18 +0530

    update git configuration for work

--- home/work.nix
+++ home/work.nix
@@ -1,11 +1,15 @@
 {
   programs.git.includes = [
     {
+      condition = "gitdir:~/Work/";
+      contents = {
+        pull.rebase = false;
+      };
+    }
+    {
       condition = "gitdir:~/Work/symbiofy/";
       contents = {
-        user = {
-          email = "avisek@symbiofy.ai";
-        };
+        user.email = "avisek@symbiofy.ai";
       };
     }
   ];
