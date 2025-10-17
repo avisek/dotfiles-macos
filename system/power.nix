e84d1e7970856fae59365d3b69203e0432846576
Avisek Das <avisekdas555@gmail.com>
2025-10-17 21:08:44 +0530

    add screensaver idle time setting to power.nix

--- system/power.nix
+++ system/power.nix
@@ -2,5 +2,6 @@
   system.activationScripts.postActivation.text = ''
     sudo pmset -b displaysleep 30
     sudo pmset -c displaysleep 60
+    /usr/bin/defaults -currentHost write com.apple.screensaver idleTime -int 0
   '';
 }
