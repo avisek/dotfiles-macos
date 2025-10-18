798a42a3c0b248d42a5f3dc8c386a571ce0408eb
Avisek Das <avisekdas555@gmail.com>
2025-10-18 21:20:32 +0530

    rename postActivation script to power in power.nix for clarity

--- system/power.nix
+++ system/power.nix
@@ -1,5 +1,5 @@
 {
-  system.activationScripts.postActivation.text = ''
+  system.activationScripts.power.text = ''
     sudo pmset -b displaysleep 30
     sudo pmset -c displaysleep 60
     /usr/bin/defaults -currentHost write com.apple.screensaver idleTime -int 0
