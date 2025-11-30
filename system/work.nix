69385d5d1d143268f195a82fbdc94cf8d301f9cd
Avisek Das <avisekdas555@gmail.com>
2025-11-30 12:27:12 +0530

    2

--- system/work.nix
+++ system/work.nix
@@ -1,8 +1,16 @@
-{pkgs, ...}: {
-  environment.systemPackages = with pkgs; [
+{
+  pkgs,
+  lib,
+  ...
+}:
+{
+  # Slack is only available on Darwin
+  environment.systemPackages = lib.optionals pkgs.stdenv.isDarwin (with pkgs; [
     slack
-  ];
-
+  ]);
+}
+# Darwin-specific Slack configuration
+// lib.optionalAttrs pkgs.stdenv.isDarwin {
   # Clear Slack data
   # rm -rf /Users/avisek/Library/Application\ Support/Slack
   # rm -rf /Users/avisek/Library/Preferences/com.tinyspeck.slackmacgap.plist
@@ -11,7 +19,7 @@
   # rm -rf /Users/avisek/Library/Caches/com.tinyspeck.slackmacgap.ShipIt
   # rm -rf /Users/avisek/Library/Caches/com.tinyspeck.slackmacgap
 
-  # Block Slack auto-update
+  # Block Slack auto-update (re-added on each rebuild)
   # TODO: Migrate to networking.hosts once nix-darwin supports it (like NixOS)
   system.activationScripts.extraActivation.text = ''
     if ! grep -q "downloads.slack-edge.com" /etc/hosts; then
@@ -19,11 +27,13 @@
     fi
   '';
 
-  # Unblock Slack domain before nix rebuild so updates can be downloaded
-  # Hook called by nrs alias in sh.nix (pattern: _pre_nrs_*)
-  environment.interactiveShellInit = ''
-    _pre_nrs_slack() {
-      sudo sed -i '' '/downloads.slack-edge.com/d' /etc/hosts
-    }
-  '';
+  # Hook to unblock Slack domain before nix rebuild
+  # Runs before nrs via /etc/nix-hooks/pre-nrs.d/ mechanism in sh.nix
+  environment.etc."nix-hooks/pre-nrs.d/slack" = {
+    mode = "0755";
+    text = ''
+      #!/bin/sh
+      sudo sed -i "" '/downloads.slack-edge.com/d' /etc/hosts
+    '';
+  };
 }
