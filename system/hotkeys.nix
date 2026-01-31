955edc0c908fd27c2e87cfdf2c99617907373cb4
Avisek Das <avisekdas555@gmail.com>
2026-02-01 01:55:41 +0530

    add hotkey for m1ddc input control and update skhd service configuration

--- system/hotkeys.nix
+++ system/hotkeys.nix
@@ -1,8 +1,12 @@
-{
+{pkgs, ...}: {
   services.skhd = {
     enable = true;
     skhdConfig = ''
+      ctrl - f18 : ${pkgs.m1ddc}/bin/m1ddc set input 17
       alt - n : nightlight toggle
     '';
   };
+
+  # Restart skhd service
+  # launchctl kickstart -k gui/$(id -u)/org.nixos.skhd
 }
