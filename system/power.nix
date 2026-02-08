2486a05d294eaf07ba4d8a1077e79c686289b109
Avisek Das <avisekdas555@gmail.com>
2026-02-08 20:16:21 +0530

    do not sleep when connected to power

--- system/power.nix
+++ system/power.nix
@@ -1,7 +1,7 @@
 {
   system.activationScripts.power.text = ''
     sudo pmset -b displaysleep 30
-    sudo pmset -c displaysleep 60
+    sudo pmset -c displaysleep 0
     /usr/bin/defaults -currentHost write com.apple.screensaver idleTime -int 0
   '';
 }
